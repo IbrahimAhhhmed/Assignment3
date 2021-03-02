559e0f7f8d1f2bea15704c9e2e6c4feefbecd86c
IbrahimAhhhmed <55740494+IbrahimAhhhmed@users.noreply.github.com>
2021-03-02 16:51:14 +0500

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
 app.use(bodyParser.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./app/models");
@@ -30,14 +28,12 @@ db.mongoose
         process.exit();
     });
 
-// simple route
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to ibrahim application." });
 });
 
 require('./app/routes/task.routes')(app);
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
